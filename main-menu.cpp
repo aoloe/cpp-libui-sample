4c36c4d9479727701ecdda38934e40569ad60ebf
ale rimoldi <ale@graphicslab.org>
2016-06-21 20:26:35 +0200

    split the menu

---
+++ main-menu.cpp
@@ -0,0 +1,58 @@
+#include "libui/ui.h"
+
+void onActionOpen(uiMenuItem *item, uiWindow *mainwin, void *data)
+{
+	char *filename;
+
+	filename = uiOpenFile(mainwin);
+	if (filename == NULL) {
+		uiMsgBoxError(mainwin, "No file selected", "Don't be alarmed!");
+		return;
+	}
+	uiMsgBox(mainwin, "File selected", filename);
+	uiFreeText(filename);
+}
+
+void onActionQuit(uiMenuItem *item, uiWindow *w, void *data)
+{
+    uiQuit();
+}
+
+
+int main()
+{
+	uiInitOptions o = {0};
+	// memset(&o, 0, sizeof (uiInitOptions)); like above, but setting 0 everywere
+	if (uiInit(&o) != NULL) {
+        return -1;
+    }
+
+    uiMenuItem *menuItem;
+
+    uiMenu *menu = uiNewMenu("File");
+
+    menuItem = uiMenuAppendItem(menu, "Open");
+	uiMenuItemOnClicked(menuItem, onActionOpen, NULL);
+
+    // uiMenuAppendQuitItem() is the only menuAppend function that triggers an action; the action needs uiOnShouldQuit to be defined and return 1
+	menuItem = uiMenuAppendQuitItem(menu);
+	uiOnShouldQuit([](void *data){return 1;}, NULL);
+
+    // set the last parameter to 1 if you have a menu
+    uiWindow *w = uiNewWindow("Hello", 320, 240, 1);
+
+	uiBox *b =uiNewVerticalBox();
+    uiBoxSetPadded(b, 1);
+    uiWindowSetChild(w, uiControl(b));
+
+    uiButton *btn = uiNewButton("Quit");
+    uiButtonOnClicked(btn, [](uiButton *b, void *data){uiQuit();}, NULL);
+    uiBoxAppend(b, uiControl(btn), 0);
+
+    // quit the application when the window closes
+	uiWindowOnClosing(w, [](uiWindow *w, void *data) {uiQuit(); return 1;}, NULL);
+
+    uiControlShow(uiControl(w));
+    uiMain();
+}
+
--- main.cpp
+++ main.cpp
@@ -1,52 +1,16 @@
 #include "libui/ui.h"
 
-int onWindowClosing(uiWindow *w, void *data)
-{
-	uiQuit();
-	return 1;
-}
-
-void onActionOpen(uiMenuItem *item, uiWindow *mainwin, void *data)
-{
-	char *filename;
-
-	filename = uiOpenFile(mainwin);
-	if (filename == NULL) {
-		uiMsgBoxError(mainwin, "No file selected", "Don't be alarmed!");
-		return;
-	}
-	uiMsgBox(mainwin, "File selected", filename);
-	uiFreeText(filename);
-}
-
-void onActionQuit(uiMenuItem *item, uiWindow *w, void *data)
-{
-    uiQuit();
-}
-
-
 int main()
 {
+    // @todo: is this correct?
 	uiInitOptions o = {0};
 	// memset(&o, 0, sizeof (uiInitOptions)); like above, but setting 0 everywere
 	if (uiInit(&o) != NULL) {
-		// abort();
         return -1;
     }
 
-    uiMenuItem *menuItem;
-
-    uiMenu *menu = uiNewMenu("File");
-    menuItem = uiMenuAppendItem(menu, "Open");
-	uiMenuItemOnClicked(menuItem, onActionOpen, NULL);
-    // menuItem = uiMenuAppendItem(menu, "Quit");
-	// uiMenuItemOnClicked(menuItem, onActionQuit, NULL);
-	menuItem = uiMenuAppendQuitItem(menu);
-	// uiOnShouldQuit(onActionQuit, NULL);
-	uiOnShouldQuit([](void *data){return 1;}, NULL); // uiMenuAppendQuitItem() is the only menuAppend function that triggers an action; the action needs uiOnShouldQuit to be defined and return 1
-
-    // uiWindow *w = uiNewWindow("Hello", 320, 240, 0); // set the last parameter to 0 if you have a menu
-    uiWindow *w = uiNewWindow("Hello", 320, 240, 1); // set the last parameter to 1 if you have a menu
+    // set the last parameter to 1 if you have a menu
+    uiWindow *w = uiNewWindow("Hello World", 320, 240, 0);
 
 	uiBox *b =uiNewVerticalBox();
     uiBoxSetPadded(b, 1);
@@ -56,8 +20,8 @@ int main()
     uiButtonOnClicked(btn, [](uiButton *b, void *data){uiQuit();}, NULL);
     uiBoxAppend(b, uiControl(btn), 0);
 
-	uiWindowOnClosing(w, onWindowClosing, NULL);
-	// uiWindowOnClosing(w, [](uiWindow *w, void *data) {uiQuit(); return 1;}, NULL);
+    // quit the application when the window closes
+	uiWindowOnClosing(w, [](uiWindow *w, void *data) {uiQuit(); return 1;}, NULL);
 
     uiControlShow(uiControl(w));
     uiMain();
